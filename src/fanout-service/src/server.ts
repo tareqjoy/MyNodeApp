bb83442651924e3a36fcd291dc31e2f1f9ef1526
tareqjoy <me.tareqjoy@gmail.com>
2024-08-11 15:56:05 +0100

    Add redis

--- src/fanout-service/src/server.ts
+++ src/fanout-service/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { Kafka, Consumer, logLevel } from 'kafkajs';
+import { createClient } from 'redis'
 import * as log4js from "log4js";
 import { router } from "./routes/fanout";
+import { newPostFanout } from "./workers/new-post"
 import bodyParser from "body-parser";
 
 const logger = log4js.getLogger();
@@ -10,7 +12,8 @@ logger.level = "trace";
 const appport = process.env.PORT || 5004;
 const kafka_client_id = process.env.KAFKA_CLIENT_ID || 'fanout';
 const kafka_host_port = process.env.KAFKA_HOST_PORT || 'localhost:9092';
-export const kafka_fanout_topic = process.env.KAFKA_FANOUT_TOPIC || 'new-post';
+const redis_host_port = process.env.REDIS_HOST_PORT || 'redis://localhost:6379';
+const kafka_fanout_topic = process.env.KAFKA_FANOUT_TOPIC || 'new-post';
 const kafka_fanout_group = process.env.KAFKA_FANOUT_GROUP || 'fanout-group';
 const api_path_root = process.env.API_PATH_ROOT || '/v1/fanout';
 
@@ -26,55 +29,78 @@ class HttpError extends Error {
   }
 }
 
-logger.info(`Will listen kafka at ${kafka_host_port}`);
-const fanoutKafka = new Kafka({
-  clientId: kafka_client_id,
-  brokers: [kafka_host_port]
-});
+async function main() {
+  logger.info(`Will listen kafka at ${kafka_host_port}`);
+  const fanoutKafka = new Kafka({
+    clientId: kafka_client_id,
+    brokers: [kafka_host_port]
+  });
 
-export const fanoutConsumer = fanoutKafka.consumer({ groupId: kafka_fanout_group });
-fanoutConsumer.connect();
-logger.info(`Connected to group: ${kafka_fanout_group}`);
-
-fanoutConsumer.subscribe({ topic: kafka_fanout_topic, fromBeginning: true});
-logger.info(`Subscribed to topic: ${kafka_fanout_topic}`);
-
-fanoutConsumer.run({
-  eachMessage: async({ topic, partition, message}) => {
-      logger.info({topic, partition, offset: message.offset, value: message.value?.toString()})
-  }
-});
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use(api_path_root, router);
-
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const error = new HttpError('Not found', 404);
-  next(error);
-});
+  const redisClient = await createClient( { url: redis_host_port })
+    .on('error', err => logger.error('Redis Client Error', err))
+    .connect();
 
 
-app.use((error: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.status(error.statusCode || 500);
-  res.json({
-    message: error
-  })
-});
 
-// Start the server and listen to the port
-app.listen(appport, () => {
-  console.log(`Server is running on port ${appport}`);
-});
+  const fanoutConsumer = fanoutKafka.consumer({ groupId: kafka_fanout_group });
+  fanoutConsumer.connect();
+  logger.info(`Connected to group: ${kafka_fanout_group}`);
 
-process.on('SIGINT', async () => {
-  try {
-    console.log('Caught interrupt signal, shutting down...');
-    fanoutConsumer.disconnect();
-    console.log(`Consumer disconnected`);
-    process.exit(0);
-  } catch (error) {
-    console.error('Error during disconnect:', error);
-  }
-});
+  fanoutConsumer.subscribe({ topic: kafka_fanout_topic, fromBeginning: true});
+  logger.info(`Subscribed to topic: ${kafka_fanout_topic}`);
+
+  fanoutConsumer.run({
+    eachMessage: async({ topic, partition, message}) => {
+        logger.trace("Kafka message received: " + {topic, partition, offset: message.offset, value: message.value?.toString()});
+        
+        if (message.value != undefined) {
+          newPostFanout(redisClient, message.value?.toString());
+        } else {
+          logger.warn(`Message value undefined`);
+        }
+    }
+  });
+
+
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+
+  app.use(api_path_root, router);
+
+  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const error = new HttpError('Not found', 404);
+    next(error);
+  });
+
+
+  app.use((error: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(error.statusCode || 500);
+    res.json({
+      message: error
+    })
+  });
+
+  // Start the server and listen to the port
+  app.listen(appport, () => {
+    console.log(`Server is running on port ${appport}`);
+  });
+
+  process.on('SIGINT', async () => {
+    try {
+      console.log('Caught interrupt signal, shutting down...');
+      fanoutConsumer.disconnect();
+      console.log(`Consumer disconnected`);
+    
+      redisClient.disconnect();
+      console.log(`Redis disconnected`);
+      process.exit(0);
+    } catch (error) {
+      console.error('Error during disconnect:', error);
+    }
+  });
+};
+
+
+main()
+
+
--- src/fanout-service/src/workers/new-post.ts
+++ src/fanout-service/src/workers/new-post.ts
@@ -1,6 +1,19 @@
-import { fanoutConsumer } from '../server'
 import * as log4js from "log4js";
+import { RedisClientType } from 'redis'
 
 const logger = log4js.getLogger();
 logger.level = "trace";
 
+export const newPostFanout = async (redisClient: RedisClientType<any, any, any>, messageStr: string) => {
+    const message = JSON.parse(messageStr);
+
+    if (message.postTime && message.postId) {
+        await redisClient.zAdd(message.userId, {
+            score: message.postTime,
+            value: message.postId
+        });
+        logger.trace(`Saved to redis: ${messageStr}`);
+    } else {
+        logger.warn(`No postTime and/or postId: ${messageStr}`);
+    }
+} 
