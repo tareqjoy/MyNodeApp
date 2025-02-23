dd2673f30b76834a13efb3ec772c46a5e580d22f
tareqjoy <me.tareqjoy@gmail.com>
2025-02-23 23:21:57 +0000

    add otel & jager. TODO: Test otel in kube

--- scripts/kubernetes/helm/helm-install.sh
+++ scripts/kubernetes/helm/helm-install.sh
@@ -3,3 +3,7 @@
 
 helm repo add fluent https://fluent.github.io/helm-charts
 helm upgrade --install -f fluent-bit/values.yml fluent-bit fluent/fluent-bit
+
+# https://opentelemetry.io/docs/platforms/kubernetes/helm/collector/
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm upgrade --install -f otel-collector/values.yml my-opentelemetry-collector open-telemetry/opentelemetry-collector --set image.repository="otel/opentelemetry-collector-k8s" --set mode=daemonset 
