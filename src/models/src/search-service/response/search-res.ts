07409bd72fa4a4c25be32ef8a8ac8ae8e3796129
tareqjoy <me.tareqjoy@gmail.com>
2025-03-15 12:51:05 +0000

    search result will have details than just ids

--- src/models/src/search-service/response/search-res.ts
+++ src/models/src/search-service/response/search-res.ts
@@ -1,11 +1,39 @@
-export class SearchRes {
-  userIds?: string[];
-  postIds?: string[];
+export class SearchResUser {
+  userid: string;
+  username: string;
+  name: string;
 
   constructor();
-  constructor(options: { userIds?: string[]; postIds?: string[] });
-  constructor(options?: { userIds?: string[]; postIds?: string[] }) {
-    this.userIds = options?.userIds;
-    this.postIds = options?.postIds;
+  constructor(userid: string, username: string, name: string);
+  constructor(userid?: string, username?: string, name?: string) {
+    this.userid = userid || "";
+    this.username = username || "";
+    this.name = name || "";
+  }
+}
+
+export class SearchResPost {
+  postid: string;
+  body: string;
+  time: number;
+
+  constructor();
+  constructor(postid: string, body: string, time: number);
+  constructor(postid?: string, body?: string, time?: number) {
+    this.postid = postid || "";
+    this.body = body || "";
+    this.time = time || 0;
+  }
+}
+
+export class SearchRes {
+  userResults?: SearchResUser[];
+  postResults?: SearchResPost[];
+
+  constructor();
+  constructor(options: { userResults?: SearchResUser[]; postResults?: SearchResPost[] });
+  constructor(options?: { userResults?: SearchResUser[]; postResults?: SearchResPost[] }) {
+    this.userResults = options?.userResults;
+    this.postResults = options?.postResults;
   }
 }
--- src/search-service/src/routes/all.ts
+++ src/search-service/src/routes/all.ts
@@ -1,7 +1,7 @@
 import { Client } from "@elastic/elasticsearch";
 import express from "express";
 import { getFileLogger } from "@tareqjoy/utils";
-import { SearchReq, InvalidRequest, SearchRes } from "@tareqjoy/models";
+import { SearchReq, InvalidRequest, SearchRes, SearchResUser, SearchResPost } from "@tareqjoy/models";
 import { plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
 
@@ -25,47 +25,26 @@ export const createAllRouter = (client: Client) => {
       res.status(400).json(new InvalidRequest(errors));
       return;
     }
-    const searchIndices: string[] = [];
-    var searchQuery = "";
-
-    if (searchReq.allToken) {
-      searchIndices.push(es_index_users);
-      searchIndices.push(es_index_posts);
-      searchQuery = searchReq.allToken;
-    } else if (searchReq.userToken) {
-      searchIndices.push(es_index_users);
-      searchQuery = searchReq.userToken;
-    } else if (searchReq.postToken) {
-      searchIndices.push(es_index_posts);
-      searchQuery = searchReq.postToken;
-    }
 
     try {
-      const searchRes = await client.search({
-        index: searchIndices,
-        query: {
-          multi_match: {
-            query: searchQuery,
-          },
-        },
-      });
-
-      const searchIds: Map<string, string[]> = new Map();
-
-      for (const item of searchRes.hits.hits) {
-        if (!searchIds.has(item._index)) {
-          searchIds.set(item._index, []);
-        }
-        const source = item._source as any;
-        searchIds.get(item._index)!.push(source.mongo_id);
+      let userResults: SearchResUser[] | undefined;
+      let postResults: SearchResPost[] | undefined;
+  
+      if (searchReq.allToken) {
+        userResults = await searchElasticInUsers(client, searchReq.allToken);
+        postResults = await searchElasticInPosts(client, searchReq.allToken);
+      } else if (searchReq.userToken) {
+        userResults = await searchElasticInUsers(client, searchReq.userToken);
+      } else if (searchReq.postToken) {
+        postResults = await searchElasticInPosts(client, searchReq.postToken);
       }
 
       res
         .status(200)
         .json(
           new SearchRes({
-            userIds: searchIds.get(es_index_users),
-            postIds: searchIds.get(es_index_posts),
+            userResults: userResults,
+            postResults: postResults,
           }),
         );
     } catch (error) {
@@ -75,3 +54,110 @@ export const createAllRouter = (client: Client) => {
   });
   return router;
 };
+
+async function searchElasticInUsers(client: Client, query: string): Promise<SearchResUser[]> {
+  try {
+    const searchResponse = await client.search(getUserSearchQuery(query));
+
+    const searchRes: SearchResUser[] = [];
+
+    for (const item of searchResponse.hits.hits) {
+      const sourceAndHighlightMerged: Record<string, any> = mergeSourceAndHighlight(item);
+      searchRes.push(new SearchResUser(sourceAndHighlightMerged["mongo_id"], sourceAndHighlightMerged["username"], sourceAndHighlightMerged["name"] ));
+    }
+
+    return searchRes;
+  } catch (error) {
+    return [];
+  }
+}
+
+async function searchElasticInPosts(client: Client, query: string): Promise<SearchResPost[]> {
+  try {
+    const searchResponse = await client.search(getPostSearchQuery(query));
+
+    const searchRes: SearchResPost[] = [];
+
+    for (const item of searchResponse.hits.hits) {
+      const sourceAndHighlightMerged: Record<string, any> = mergeSourceAndHighlight(item);
+      searchRes.push(new SearchResPost(sourceAndHighlightMerged["mongo_id"], sourceAndHighlightMerged["body"], sourceAndHighlightMerged["time"] ));
+    }
+
+    return searchRes;
+  } catch (error) {
+    return [];
+  }
+}
+
+const mergeSourceAndHighlight = (result: any): Record<string, any> => {
+  const merged: Record<string, any> = { ...result._source };
+
+  if (result.highlight) {
+    for (const [key, value] of Object.entries(result.highlight)) {
+      // Assert that value is of type string[]
+      const highlightValue = value as string[];
+
+      // Replace _source field if it's present in highlight, otherwise add it
+      merged[key] = highlightValue[0]; // Taking the first highlighted fragment
+    }
+  }
+
+  return merged;
+};
+
+const getUserSearchQuery = (query: string) => ({
+  index: es_index_users,
+  body: {
+    query: {
+      bool: {
+        should: [
+          { match: { name: { query, fuzziness: "AUTO" } } },
+          { match_phrase: { username: query } }
+        ],
+        minimum_should_match: 1
+      }
+    },
+    highlight: {
+      fields: {
+        name: { 
+          fragment_size: 15,
+          number_of_fragments: 1,
+        },
+        username: {} // Enable highlighting for username
+      },
+      pre_tags: ["<mark>"],
+      post_tags: ["</mark>"]
+    },
+    size: 10
+  }
+});
+
+/**
+ * Get Post Search Query
+ * Partial match for `body`
+ */
+const getPostSearchQuery = (query: string) => ({
+  index: es_index_posts,
+  body: {
+    query: {
+      multi_match: {
+        query,
+        type: "best_fields",
+        fields: ["body"],
+        operator: "or",
+        fuzziness: "AUTO"
+      }
+    },
+    highlight: {
+      fields: {
+        "*": { 
+          fragment_size: 15,
+          number_of_fragments: 1,
+        }
+      },
+      pre_tags: ["<mark>"],
+      post_tags: ["</mark>"]
+    },
+    size: 10
+  }
+});
