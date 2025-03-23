21893eeef7e489d40ecf82f5a6e412497ced2e37
tareqjoy <me.tareqjoy@gmail.com>
2025-03-23 11:46:51 +0000

    Add demo flink kafka

--- src/analytics/app/src/main/java/analytics/App.java
+++ src/analytics/app/src/main/java/analytics/App.java
@@ -3,12 +3,35 @@
  */
 package analytics;
 
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    public void getGreeting() throws Exception {
+        // 1. get stream execution env
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 1. read kafka message as stream input
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("post-like")
+                .setGroupId("analytics-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        // Execute the Flink job
+        DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        stream.print();
+        env.execute("Kafka Sink To Gen2");
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws Exception {
+        new App().getGreeting();
     }
 }
