70f523d3080a8cf9d814c91f8f31ddd161d52009
tareqjoy <me.tareqjoy@gmail.com>
2025-08-03 11:25:32 +0100

    add neo4j install script

---
+++ scripts/installation/neo4j/neo4j.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Step 1: Download the key and store it in /etc/apt/keyrings
+curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/neo4j.gpg
+
+# Step 2: Add the Neo4j APT repository
+echo "deb [signed-by=/etc/apt/keyrings/neo4j.gpg] https://debian.neo4j.com stable 5" | sudo tee /etc/apt/sources.list.d/neo4j.list
+
+# Step 3: Update and install
+sudo apt update
+sudo apt install neo4j
+
+yes | sudo cp -rf "${SCRIPT_DIR}/neo4j.conf" "/etc/neo4j/"
