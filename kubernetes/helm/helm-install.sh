df8a9d75947c06af0fe8ed09f64519ae145ef8df
tareqjoy <me.tareqjoy@gmail.com>
2026-01-24 16:57:59 +0000

    Fix otel logs are not being sent to loki

--- kubernetes/helm/helm-install.sh
+++ kubernetes/helm/helm-install.sh
@@ -6,8 +6,4 @@ helm upgrade --install -f fluent-bit/values.yml fluent-bit fluent/fluent-bit
 
 # https://opentelemetry.io/docs/platforms/kubernetes/helm/collector/
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm upgrade --install my-opentelemetry-collector open-telemetry/opentelemetry-collector \                                                                                                                       ─╯
-  -n default \
-  --set mode=daemonset \
-  --set image.repository=otel/opentelemetry-collector-contrib \
-  --set image.tag=0.122.0
+helm upgrade --install -f otel-collector/values.yml my-opentelemetry-collector open-telemetry/opentelemetry-collector --set mode=daemonset --set image.repository=otel/opentelemetry-collector-contrib --set image.tag=0.122.0
